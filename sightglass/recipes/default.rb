eb321b34dc6fcd82d55ca6eb4db8a5bdf81c5565
Joshua Ma <josh@benchling.com>
2015-02-12 14:46:33 -0800

    Try again

--- sightglass/recipes/default.rb
+++ sightglass/recipes/default.rb
@@ -7,7 +7,7 @@
 #
 
 include_recipe 'java'
-include_recipe 'elasticsearch::default'
+include_recipe 'elasticsearch'
 # include_recipe 'elasticsearch::monit'
 include_recipe 'elasticsearch::aws'
 # include_recipe 'elasticsearch::data'

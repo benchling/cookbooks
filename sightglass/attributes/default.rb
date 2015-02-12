b86953ccff61606a3299e03cb43d096cecaa6ffc
Joshua Ma <josh@benchling.com>
2015-02-12 15:05:31 -0800

    Fix aws plugin version

--- sightglass/attributes/default.rb
+++ sightglass/attributes/default.rb
@@ -1,4 +1,5 @@
-default.java.jdk_version = 7
-default.elasticsearch.version = '1.4.3'
-# default.elasticsearch.discovery.type = 'ec2'
-# default.elasticsearch.discovery.ec2.groups = 'ElasticSearchSG'
+default.java['jdk_version'] = 7
+default.elasticsearch['version'] = '1.4.3'
+default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
+default.elasticsearch['discovery']['type'] = 'ec2'
+default.elasticsearch['discovery']['ec2']['groups'] = 'ElasticSearchSG'

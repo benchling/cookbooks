d506acec4e25be9177b2bde42cfa6476dfd30050
Joshua Ma <josh@benchling.com>
2015-02-12 13:12:53 -0800

    Set some attributes

--- sightglass/attributes/default.rb
+++ sightglass/attributes/default.rb
@@ -1,1 +1,4 @@
-default['java']['jdk_version'] = 7
+default.java.jdk_version = 7
+default.elasticsearch.version = '1.4'
+default.elasticsearch.discovery.type = 'ec2'
+default.elasticsearch.discovery.ec2.groups = 'ElasticSearchSG'
--- sightglass/recipes/default.rb
+++ sightglass/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 #
-# 
+#
 
 include_recipe 'java'
 include_recipe 'elasticsearch::default'

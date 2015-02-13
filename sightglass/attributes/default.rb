31f4495368474ec933995af70fc3fd11811dc98e
Joshua Ma <josh@benchling.com>
2015-02-12 19:27:11 -0800

    Set node and cluster names from opsworks

--- sightglass/attributes/default.rb
+++ sightglass/attributes/default.rb
@@ -3,3 +3,6 @@ default.elasticsearch['version'] = '1.4.3'
 default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
 default.elasticsearch['discovery']['type'] = 'ec2'
 default.elasticsearch['discovery']['ec2']['groups'] = 'ElasticSearchSG'
+
+default.elasticsearch['cluster']['name'] = 'es.' + node['opsworks']['stack']['name']
+default.elasticsearch['node']['name'] = node['opsworks']['instance']['hostname']

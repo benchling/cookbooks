26496fad945a7214a9e7e800d6336c84fb9f4dd7
Joshua Ma <josh@benchling.com>
2015-02-13 12:55:09 -0800

    Configure minimum_master_nodes to 2

--- sightglass/recipes/default.rb
+++ sightglass/recipes/default.rb
@@ -1,10 +1,4 @@
-#
-# Cookbook Name:: sightglass
-# Recipe:: default
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
-#
-#
+# Sets up elasticsearch for sightglass.
 
 include_recipe 'java'
 include_recipe 'elasticsearch'

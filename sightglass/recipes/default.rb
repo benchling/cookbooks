aa66745260777951faee410e4b7ed604d4eef0f7
Joshua Ma <josh@benchling.com>
2015-02-12 21:08:20 -0800

    Add ES_HEAP_SIZE env

--- sightglass/recipes/default.rb
+++ sightglass/recipes/default.rb
@@ -12,3 +12,8 @@ include_recipe 'elasticsearch'
 include_recipe 'elasticsearch::aws'
 # include_recipe 'elasticsearch::data'
 # include_recipe 'elasticsearch::ebs'
+
+# Half of memory towards ES heap.
+env 'ES_HEAP_SIZE' do
+    value node.memory.total.to_i / 2
+end

d31de03c5042f8e58b946c4696472575aabb5640
Joshua Ma <josh@benchling.com>
2016-02-23 00:04:30 -0800

    Enhance groovy script to allow required_field_length
    Summary:
    - Takes in required_field_length param to make match stricter (only true if
      length of source_field matches required_field_length)
    - Handles circular sequences by dividing length by 2
    
    Ref T4226
    
    Reviewers: somak
    
    Reviewed By: somak
    
    Maniphest Tasks: T4226
    
    Differential Revision: https://phab.benchling.com/D2676

--- sightglass/files/default/source_regex.groovy
+++ sightglass/files/default/source_regex.groovy
@@ -1,4 +1,20 @@
-// Expects `_source` to be available from elasticsearch.
-// Expects `source_field` param to look up from _source.
-// Expects `regex` param to match against.
-(_source[source_field] =~ regex).find()
+/*
+Expects `_source` to be available from elasticsearch.
+Parameters:
+    `source_field` param to look up from _source.
+    `regex` param to match against.
+    `required_field_length` param - null if no requirement, integer to specify required length.
+ */
+match = _source[source_field] =~ regex
+isMatch = match.find()
+// TODO(T4024): use match.start() and match.end() to remove false positives.
+if (!isMatch || required_field_length == null) {
+    return isMatch
+}
+
+sourceFieldLength = _source[source_field].size()
+// Circular sequences have bases doubled and need to have length divided by 2.
+if (_source.circular) {
+    sourceFieldLength /= 2
+}
+return sourceFieldLength == required_field_length

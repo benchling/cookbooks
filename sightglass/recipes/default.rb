2346119feb8a16fab37c6d2e7deb5f6099fe6532
Joshua Ma <josh@benchling.com>
2018-04-28 20:04:09 -0700

    Revert "Disable foxpass until terraform changes roll out"
    This reverts commit 8a70505648b09d4d50e55e7ba45f2e619956e10c.

--- sightglass/recipes/default.rb
+++ sightglass/recipes/default.rb
@@ -41,26 +41,26 @@ cookbook_file '/usr/local/etc/elasticsearch/scripts/sort_value.groovy' do
   action :create
 end
 
-# cookbook_file '/etc/sudoers.d/20-foxpass' do
-#   source '20-foxpass'
-#   owner 'root'
-#   group 'root'
-#   mode '0440'
-#   action :create
-# end
+cookbook_file '/etc/sudoers.d/20-foxpass' do
+  source '20-foxpass'
+  owner 'root'
+  group 'root'
+  mode '0440'
+  action :create
+end
 
-# foxpass_secrets = data_bag_item("sightglass", "foxpass")
+foxpass_secrets = data_bag_item("sightglass", "foxpass")
 
-# bash 'install_foxpass' do
-#   user 'root'
-#   cwd '/tmp'
-#   code <<-EOH
-#       wget https://raw.githubusercontent.com/foxpass/foxpass-setup/master/linux/ubuntu/14.04/foxpass_setup.py
-#       python foxpass_setup.py --base-dn dc=benchling,dc=com --bind-user linux --bind-pw #{foxpass_secrets["bind_pw"]} \
-#           --api-key "#{foxpass_secrets["api_key"]}"
-#       rm foxpass_setup.py
-#       mkdir -p /opt/chef-foxpass/
-#       touch /opt/chef-foxpass/done
-#       EOH
-#   not_if { ::File.exists?('/opt/chef-foxpass/done') }
-# end
+bash 'install_foxpass' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+      wget https://raw.githubusercontent.com/foxpass/foxpass-setup/master/linux/ubuntu/14.04/foxpass_setup.py
+      python foxpass_setup.py --base-dn dc=benchling,dc=com --bind-user linux --bind-pw #{foxpass_secrets["bind_pw"]} \
+          --api-key "#{foxpass_secrets["api_key"]}"
+      rm foxpass_setup.py
+      mkdir -p /opt/chef-foxpass/
+      touch /opt/chef-foxpass/done
+      EOH
+  not_if { ::File.exists?('/opt/chef-foxpass/done') }
+end

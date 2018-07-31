a7bc765ac8139700fb54377695163f12e466dad2
Vineet Gopal <vineet@benchling.com>
2018-07-30 18:17:26 -0700

    Improve sort_value script to handle doc and subfields
    Summary:
    - Adds a required `field_base` key to specify which base to find
      the field. Currently supports `doc` and `_source`, and defaults
      to `_source` otherwise.
    - Adds an optional `subfields` key to allow accessing child keys.
      This is necessary for items like `_uid` and `_type`, which are
      arrays of strings and need `subfields=[0]` passed in.
    
    Test Plan:
    - I downloaded the script locally by pushing this change to a local branch,
      and changing local_dev/docker/sightglass/Dockerfile to pull from my branch
      instead. I then ran tests using the updated script.
    
    Reviewers: somak
    
    Reviewed By: somak
    
    Subscribers: josh
    
    Differential Revision: https://phab.benchling.com/D18590

--- sightglass/files/default/sort_value.groovy
+++ sightglass/files/default/sort_value.groovy
@@ -1,10 +1,18 @@
 def value = null
 
+
+if (field_base == 'doc') {
+  base = doc
+} else {
+  assert field_base == '_source'
+  base = _source
+}
+
 if (!sort_field.contains('.')) {
-  // For top-level fields, use the value from _source.
+  // For top-level fields, use the value from base directly.
   // Using doc[sort_field] results in a faster search since _source is not loaded into memory,
   // but unfortunately does not work for analyzed fields.
-  value = _source[sort_field]
+  value = base[sort_field]
 } else {
   // For nested fields, apply the nested_filter and use the value from the field that matches
   // the nested filter.
@@ -12,11 +20,18 @@ if (!sort_field.contains('.')) {
   // sort spec. Setting `nested_path` causes script not to be run; `nested_filter` doesn't
   // properly filter all documents. A future version of ES may support this.
   (nested_path, nested_field) = sort_field.tokenize('.')
-  for (nested_source in _source[nested_path]) {
+  for (nested_source in base[nested_path]) {
    if (nested_source[nested_filter_field] == nested_filter_value) {
      value = nested_source[nested_field]
      break
    }
+  }
+}
+// If `subfields` are specified, iteratively grab children using the specified keys. This
+// works for array accesses (subfields=[0]) or dictionary accesses (subfields=['key']).
+if (binding.hasVariable('subfields')) {
+  for (subfield in subfields) {
+    value = value[subfield]
   }
 }
 // If no value is set, return a value such that the document is sorted last.
